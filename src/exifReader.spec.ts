0130d3813953367e3d9a2a1f125fe19ae3ea51d0
Kevin Peters <kwpeters@gmail.com>
2016-11-23 22:58:11 -0500

    Ported exifReader.

---
+++ src/exifReader.spec.ts
@@ -0,0 +1,39 @@
+import * as tape from "tape";
+import {getCreateDate} from "./exifReader";
+
+
+
+tape("exifReader", function (t: tape.Test): void {
+
+    t.test("getCreateDate", function (t: tape.Test): void {
+
+
+        t.test("will read the creation date from an iPhone imae", function (t: tape.Test): void {
+            getCreateDate("test/input/2015-03-11 09.05.32.jpg")
+                .then((creationDatestamp) => {
+                    t.equal(creationDatestamp.toString(), "2015-03-11");
+                    t.end();
+                });
+        });
+
+
+        t.test("will read the creation date from a Canon imae", function (t: tape.Test): void {
+            getCreateDate("test/input/IMG_8718-canon.JPG")
+                .then((creationDatestamp) => {
+                    t.equal(creationDatestamp.toString(), "2015-12-30");
+                    t.end();
+                });
+        });
+
+
+        t.test("will read the creation date from a Panasonic imae", function (t: tape.Test): void {
+            getCreateDate("test/input/P1040165-panasonic.JPG")
+                .then((creationDatestamp) => {
+                    t.equal(creationDatestamp.toString(), "2015-12-30");
+                    t.end();
+                });
+        });
+
+    });
+
+});
---
+++ src/exifReader.ts
@@ -0,0 +1,47 @@
+import {Datestamp} from "./datestamp";
+
+// No .d.ts file available for exif package.
+// tslint:disable-next-line:variable-name no-require-imports no-var-requires
+const ExifImage: any = require("exif").ExifImage;
+
+
+const dateRegex: RegExp = /(\d{4}):(\d{2}):(\d{2})/;
+
+/**
+ * Gets a Datestamp for the specified image file.
+ * @param {string} imageFile - The path to the file to query
+ * @returns {Promise<Datestamp>} A Promise for a Datestamp corresponding to the
+ * CreateDate within the image's exif data.  The Promise will reject if it could
+ * not be obtained.
+ */
+export function getCreateDate(imageFile: string): Promise<Datestamp> {
+    "use strict";
+
+    return new Promise<Datestamp>((resolve, reject) => {
+
+        try {
+            // tslint:disable-next-line:no-unused-new
+            new ExifImage({image: imageFile}, (err: any, exifData: any) => {
+
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                const matches: RegExpExecArray | null = dateRegex.exec(exifData.exif.CreateDate);
+                if (matches) {
+                    const datestamp: Datestamp = Datestamp.fromYMD(
+                        parseInt(matches[1], 10), parseInt(matches[2], 10), parseInt(matches[3], 10));
+                    resolve(datestamp);
+                } else {
+                    reject("Unknown CreateDate string format");
+                }
+
+            });
+        } catch (error) {
+            reject(error);
+        }
+
+    });
+
+}
